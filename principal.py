88575a2751994edc86efa82a502e5b0d47443f7d
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-09-29 19:57:41 -0300

    fdsfds

--- principal.py
+++ principal.py
@@ -14,7 +14,7 @@ nomes = [] #lista para guardar os nomes
 resposta = ''
 while resposta != 'SAIR':
   #resposta = input('Digite o nome: ').upper()
-  st.text_input('Digite o nome: ',key=resposta)
+  resposta = st.text_input('Digite o nome: ')
   nomes.append(resposta)#colocou em ultimo
   ordena(nomes)
   st.write(nomes)
